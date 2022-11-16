d6cf0e054f11b63dd57293d2132808a720900e5f
Devin Jaggernauth <neexis@gmail.com>
2022-11-15 22:54:05 -0400

    clear card data on popup close

--- pages/collection.tsx
+++ pages/collection.tsx
@@ -31,6 +31,7 @@ const Collection = (): JSX.Element => {
     document.body.style.position = '';
     document.body.style.top = '';
     window.scrollTo(0, parseInt(scrollY || '0') * -1);
+    setCardData(null);
   };
 
   const handleOutsideClick = (event: MouseEvent<HTMLDivElement>): void => {
