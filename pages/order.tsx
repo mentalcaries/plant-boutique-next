2ae02519d2c7bfa7c82b0ba7e28375f2b3411a2c
Devin Jaggernauth <neexis@gmail.com>
2022-11-16 09:59:12 -0400

    update order background

--- pages/order.tsx
+++ pages/order.tsx
@@ -14,6 +14,8 @@ const Order = () => {
       exit={{ opacity: 0, transition: { duration: 0.3 } }}
     >
       <div className="order__content">
+        {/* Photo by <a href="https://unsplash.com/es/@magali_merzougui?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Magali Merzougui</a> on <a href="https://unsplash.com/s/photos/houseplant?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a> */}
+
         <h2 className="order__title">How To Order</h2>
         {/* <p className="order__subtitle">We're still growing!</p> */}
         <p className="order__subtitle">
